99e3e122011767fe6a106a06588b17c0c6f03059
krztom <tomecki.k@gmail.com>
2018-12-07 21:24:22 +0100

    - block scoping

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('test');
+function hello() {
+  let message= "Hello!";
+  console.log(message);
+}
+function greeting() {
+    let message = "How are you?";
+    console.log(message);
+    
+}
+
+hello();
+greeting();
