ef5ce22f904859d5623f9d0cb22c911df5d538de
krztom <tomecki.k@gmail.com>
2018-12-09 13:13:25 +0100

    update

--- app/index.js
+++ app/index.js
@@ -10,3 +10,21 @@ function greeting() {
 
 hello();
 greeting();
+
+let a = 'good';
+let c = `${a} moring`;
+console.log( c );
+
+let one = [20,30,40];
+let two = [10, ...one, 50];
+console.log( two );
+
+
+function collect(...three) {
+    console.log( three );
+    
+}
+
+collect( 5,1,6,7,9,12,99,51);
+
+
