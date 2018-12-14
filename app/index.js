bfb4108f8c40d222d7274113548c7c83b999fad3
krztom <tomecki.k@gmail.com>
2018-12-14 16:03:10 +0100

    working commit

--- app/index.js
+++ app/index.js
@@ -27,4 +27,48 @@ function collect(...three) {
 
 collect( 5,1,6,7,9,12,99,51);
 
+// arrow functions
 
+let cheer = () => {
+    console.log( "whooo!");
+}
+
+cheer();
+
+let values = [20,40,60];
+
+let doubled = values.map( (n) =>  n*2 );
+console.log(doubled);
+
+let points = [2,10,22,15,19,1,29];
+let hightscore = points.filter( (n) => n > 15 )
+console.log( hightscore )
+
+//repat method
+
+let re = `woo${ "oo".repeat( 50 ) }`;
+console.log( re );
+
+let search = "butterfly";
+
+console.log( search.startsWith( "butter" ) );
+console.log( search.endsWith( "butter" ) );
+console.log( search.includes( "ter" ) );
+
+
+// modules
+
+import multiply from './math';
+console.log( multiply( 5,10 ));
+
+class Calculator {
+    static multiply_1( a,b ) {
+        return a*b;
+    }
+
+    static add_1( a,b ) {
+        return a+b;
+    }
+};
+let math_1 = Calculator.multiply_1( 9,10);
+console.log( math_1 );
---
+++ app/math.js
@@ -0,0 +1,9 @@
+const add = ( a,b ) => {
+    return a+b;
+}
+
+const multiply = ( a,b ) => a*b;
+
+export { add, multiply };
+export default multiply;
+
